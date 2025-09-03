66eb69c30030d98e5465a0e2bb66ce5be93d87fe
Vijay <vijaymurugan177@gmail.com>
2025-09-03 16:57:49 +0530

    button

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App fixed">
-      <StorageCard
+      {/* <StorageCard
         cardIndex={0}
         storageAmount="847GB"
         storageTotal="1TB"
         percentage={84.7}
         imageSrc="/Image.png"
-      />
+      /> */}
       <StorageCard
         cardIndex={1}
         storageAmount="847GB"
@@ -20,15 +20,15 @@ function App() {
         imageSrc="/Image.png"
       />
 
-      <StorageCard
+      {/* <StorageCard
         cardIndex={2}
         storageAmount="247GB"
         storageTotal="1TB"
         percentage={29.7}
         imageSrc="/Image.png"
-      />
+      /> */}
 
-      {/* <div className="card-4">X</div> */}
+      <div className="button">Lable</div>
 
       {/* <div class="wrapper">
         <div class="content">Your content here</div>
